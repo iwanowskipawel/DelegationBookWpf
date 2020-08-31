5323cbce96f553ba123ce2de935076cef4cbcc74
pawel.iwanowski <iwanowski314@gmail.com>
2020-08-31 08:27:20 +0200

    Separate AttriubuteImporeter from GridDisplayExtender

---
+++ Delegation/AttributeImporter.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace Delegation
+{
+    public static class AttributeImporter 
+    { 
+        public static string GetDisplayName(object instance, PropertyInfo propertyInfo)
+        {
+            string output = "";
+            var attr = GetDisplayAttribute(instance, propertyInfo.Name);
+            if (attr != null)
+            {
+                output = attr.Name;
+            }
+            return output;
+        }
+
+        private static DisplayAttribute GetDisplayAttribute(object instance, string name)
+        {
+            DisplayAttribute output;
+
+            var attributeType = typeof(DisplayAttribute);
+            var prop = instance.GetType().GetProperty(name);
+
+            output = (DisplayAttribute)prop.GetCustomAttributes(attributeType, false).FirstOrDefault();
+
+            return output;
+        }
+    }
+}
--- Delegation/GridDisplayExtender.cs
+++ Delegation/GridDisplayExtender.cs
@@ -1,9 +1,7 @@
 ﻿using DelegationLibrary.Model;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Reflection;
 using System.Windows.Controls;
 using System.Windows.Data;
 
@@ -13,11 +11,11 @@ namespace Delegation
     {
         public static void DisplayInGrid(this IEnumerable<IBusinessTrip> trips, DataGrid dataGrid)
         {
+            dataGrid.Columns.Clear();
             dataGrid.AutoGenerateColumns = false;
-            var tripInstance = trips.FirstOrDefault();
-            ObservableCollection<IBusinessTrip> tripsObs = new ObservableCollection<IBusinessTrip>(trips);
 
-            var tripProperties = tripInstance.GetType().GetProperties();
+            var defaultTrip = trips.FirstOrDefault();
+            var tripProperties = defaultTrip.GetType().GetProperties();
 
             dataGrid.ItemsSource = trips;
 
@@ -25,34 +23,11 @@ namespace Delegation
             {
                 DataGridTextColumn col = new DataGridTextColumn()
                 {
-                    Header = GetDisplayName(tripInstance, property),
+                    Header = AttributeImporter.GetDisplayName(defaultTrip, property),
                     Binding = new Binding(property.Name)
                 };
                 dataGrid.Columns.Add(col);
             }
-        }
-
-        private static string GetDisplayName(object instance, PropertyInfo propertyInfo)
-        {
-            string output = "";
-            var attr = GetDisplayAttribute(instance, propertyInfo.Name);
-            if (attr != null)
-            {
-                output = attr.Name;
-            }
-            return output;
-        }
-
-        private static DisplayAttribute GetDisplayAttribute(object instance, string name)
-        {
-            DisplayAttribute output;
-
-            var attributeType = typeof(DisplayAttribute);
-            var prop = instance.GetType().GetProperty(name);
-
-            output = (DisplayAttribute)prop.GetCustomAttributes(attributeType, false).FirstOrDefault();
-
-            return output;
         }
     }
 }
