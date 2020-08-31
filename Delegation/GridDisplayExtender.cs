03ed1c88b063c234576210ae44d5a557239efdea
pawel.iwanowski <iwanowski314@gmail.com>
2020-08-31 08:15:26 +0200

    Create DisplayInGrid extension method to IEnumerable<> and move to GridDisplayExtender class

---
+++ Delegation/GridDisplayExtender.cs
@@ -0,0 +1,58 @@
+﻿using DelegationLibrary.Model;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Delegation
+{
+    public static class GridDisplayExtender
+    {
+        public static void DisplayInGrid(this IEnumerable<IBusinessTrip> trips, DataGrid dataGrid)
+        {
+            dataGrid.AutoGenerateColumns = false;
+            var tripInstance = trips.FirstOrDefault();
+            ObservableCollection<IBusinessTrip> tripsObs = new ObservableCollection<IBusinessTrip>(trips);
+
+            var tripProperties = tripInstance.GetType().GetProperties();
+
+            dataGrid.ItemsSource = trips;
+
+            foreach (var property in tripProperties)
+            {
+                DataGridTextColumn col = new DataGridTextColumn()
+                {
+                    Header = GetDisplayName(tripInstance, property),
+                    Binding = new Binding(property.Name)
+                };
+                dataGrid.Columns.Add(col);
+            }
+        }
+
+        private static string GetDisplayName(object instance, PropertyInfo propertyInfo)
+        {
+            string output = "";
+            var attr = GetDisplayAttribute(instance, propertyInfo.Name);
+            if (attr != null)
+            {
+                output = attr.Name;
+            }
+            return output;
+        }
+
+        private static DisplayAttribute GetDisplayAttribute(object instance, string name)
+        {
+            DisplayAttribute output;
+
+            var attributeType = typeof(DisplayAttribute);
+            var prop = instance.GetType().GetProperty(name);
+
+            output = (DisplayAttribute)prop.GetCustomAttributes(attributeType, false).FirstOrDefault();
+
+            return output;
+        }
+    }
+}
--- Delegation/MainWindow.xaml.cs
+++ Delegation/MainWindow.xaml.cs
@@ -1,18 +1,12 @@
 ﻿using DelegationLibrary.DataAccess;
-using DelegationLibrary.Model;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel.DataAnnotations;
 using System.DirectoryServices.ActiveDirectory;
-using System.Linq;
-using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -35,7 +29,8 @@ namespace Delegation
             InitializeComponent();
 
             DisplayFakeDataInTextBox();
-            DisplayFakeDataInDataGrid();
+
+            _dataCollection.BusinessTrips.DisplayInGrid(dataGrid);
         }
 
         private void SetupApplication()
@@ -55,50 +50,6 @@ namespace Delegation
                     textBox.Text += $"{ t.DepartureDate } - { t.ArrivalDate } do { t.Destination.Name } - ilość przejechanych km: { t.Distance }\n";
                 }
             }
-        }
-
-        private void DisplayFakeDataInDataGrid()
-        {
-            dataGrid.AutoGenerateColumns = false;
-            var tripInstance = _dataCollection.BusinessTrips.FirstOrDefault();
-            ObservableCollection<IBusinessTrip> trips = new ObservableCollection<IBusinessTrip>(_dataCollection.BusinessTrips);
-
-            var tripProperties = tripInstance.GetType().GetProperties();
-
-            dataGrid.ItemsSource = trips;
-
-            foreach (var property in tripProperties)
-            {
-                DataGridTextColumn col = new DataGridTextColumn()
-                {
-                    Header = GetDisplayName(tripInstance, property),
-                    Binding = new Binding(property.Name)
-                };
-                dataGrid.Columns.Add(col);
-            }
-        }
-
-        private string GetDisplayName(object instance, PropertyInfo propertyInfo)
-        {
-            string output = "";
-            var attr = GetDisplayAttribute(instance, propertyInfo.Name);
-            if (attr != null)
-            {
-                output = attr.Name;
-            }
-            return output;
-        }
-
-        private DisplayAttribute GetDisplayAttribute(object instance, string name)
-        {
-            DisplayAttribute output;
-
-            var attributeType = typeof(DisplayAttribute);
-            var prop = instance.GetType().GetProperty(name);
-
-            output = (DisplayAttribute)prop.GetCustomAttributes(attributeType, false).FirstOrDefault();
-
-            return output;
         }
 
         private void textBox_TextChanged(object sender, TextChangedEventArgs e)
