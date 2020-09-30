0724d98d9189459ea62fc95fe62b70dfc14e3219
pawel.iwanowski <iwanowski314@gmail.com>
2020-09-30 15:44:11 +0200

    Change type of IDataCollection members to ICollection interface

--- DataAccessLibrary/DataAccess/IDataCollection.cs
+++ DataAccessLibrary/DataAccess/IDataCollection.cs
@@ -1,18 +1,19 @@
 ﻿using DelegationLibrary.Model;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DelegationLibrary.DataAccess
 {
     public interface IDataCollection
     {
-        IEnumerable<IAddress> Addresses { get; set; }
-        IEnumerable<IBusinessTrip> BusinessTrips { get; set; }
-        IEnumerable<ICar> Cars { get; set; }
-        IEnumerable<ICompany> Companies { get; set; }
-        IEnumerable<IDestination> Destinations { get; set; }
-        IEnumerable<IDriver> Drivers { get; set; }
-        IEnumerable<IEmployee> Employees { get; set; }
-        IEnumerable<IKilometersCard> KilometersCards { get; set; }
-        IEnumerable<IProject> Projects { get; set; }
+        ICollection<IAddress> Addresses { get; set; }
+        ICollection<IBusinessTrip> BusinessTrips { get; set; }
+        ICollection<ICar> Cars { get; set; }
+        ICollection<ICompany> Companies { get; set; }
+        ICollection<IDestination> Destinations { get; set; }
+        ICollection<IDriver> Drivers { get; set; }
+        ICollection<IEmployee> Employees { get; set; }
+        ICollection<IKilometersCard> KilometersCards { get; set; }
+        ICollection<IProject> Projects { get; set; }
     }
 }
